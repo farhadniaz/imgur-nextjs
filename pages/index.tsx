de99d40345206d1f84b8658abc0714ed2a08e6c6
Farhad Azarbarzinniaz <farhad00niaz@gmail.com>
2020-11-14 02:48:38 +0330

    home page with gallery grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,42 @@
-import { NextPage } from "next";
-import Container from '~/components/Container';
+import React, { useState } from "react";
+import { NextPage, NextPageContext } from "next";
 interface IProps {
 }
+import { getGalleryImages } from "~/services/imgur";
+import Header from "~/components/Header";
+import SpinnerLoader from "~/components/SpinnerLoader";
+import GalleryActions from "~/store/gallery/actions";
+import GalleryGrid from "~/components/GalleryGrid";
+import { MainLoaderWrapperStyle } from "~/components/GalleryGrid/style";
+import useDoFilter from "~/components/GalleryGrid/Filter/useDoFilter";
+
 const HomePage: NextPage<IProps> = (props) => {
+  const [loading, setLoading] = useState(false);
+
+  const withFilter = useDoFilter(setLoading);
+
+  const doFilter = (filterData) => {
+    withFilter(filterData);
+  };
+
   return (
-    <div>
-      <Container>Home Page</Container>
-    </div>
+    <>
+      <Header onFilter={doFilter} filterLoading={loading} />
+      <GalleryGrid />
+      {loading ? <MainLoaderWrapperStyle>
+        <SpinnerLoader className="main-loader" />
+      </MainLoaderWrapperStyle> : null}
+    </>
   );
+};
+
+HomePage.getInitialProps = async (context: NextPageContext) => {
+  const { store } = context;
+  const { data, page } = await getGalleryImages();
+  store.dispatch({
+    type: GalleryActions.SET_ITEMS,
+    payload: data,
+  });
+  return {};
 };
 export default HomePage;
