11c851f0220ac9aa3f92de0b68dc6bead39e1b5a
Farhad Azarbarzinniaz <farhad00niaz@gmail.com>
2020-11-14 02:01:36 +0330

    colors and assets

---
+++ assets/style/color.ts
@@ -0,0 +1,8 @@
+export const blackMoa = "#333333";
+export const blackTea = "#171717";
+export const gray = "#cccccc";
+export const graySilver = "#efefef";
+export const graySolar = "#f0f0f0";
+export const grayAlpha = "#f8fafb";
+export const grayMore = "#eef3f8";
+export const blueLight = "#1e8df8";
